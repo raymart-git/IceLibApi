045615a5953fc8b0b1ce6e53afc0c041500c2bad
Raymart <santiagoraymart1992@gmail.com>
2021-03-26 18:42:06 +0800

    Initial Commit on IceLibAPI, added swagger

---
+++ IceLibAPI/IceLibAPI/Controllers/InboundController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceLibAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InboundController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> TestInbound()
+        {
+            await Task.Delay(1);
+            return Ok("inbound");
+        }
+    }
+}
---
+++ IceLibAPI/IceLibAPI/Controllers/OutboundController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceLibAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OutboundController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> TestOutbound()
+        {
+            await Task.Delay(1);
+            return Ok("outbound");
+        }
+    }
+}
